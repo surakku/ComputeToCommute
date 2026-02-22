b7ee9cea971af6bc2d33bfa38b3d2869b627d076
Joooo15 <jobrandon2004@gmail.com>
2026-02-22 17:57:07 -0600

    .

--- frontend/computetocommute/vite.config.ts
+++ frontend/computetocommute/vite.config.ts
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+    server: {
+    proxy: {
+      // Proxy requests that start with '/api'
+      '/api': {
+        target: 'http://localhost:5000', // Your local API server's address
+        changeOrigin: true, // Needed for virtual hosted sites
+        secure: false, // Set to true if your API uses HTTPS
+        // rewrite: (path) => path.replace(/^\/api/, ''), // Optional: rewrite the path if your API doesn't use the /api prefix
+      },
+    },
+  },
 })
